2a4a7b7a3c38fdf5a9ac783eb8c3a2e2760bf2f7
Jack Burgess <jack@jwburgess.co.uk>
2020-02-05 18:09:41 +0000

    Added warning message when uploading from iPhone

--- groups/submit-picture.php
+++ groups/submit-picture.php
@@ -88,6 +88,9 @@ else{
 								</div>
 								<input type="text" name="answer" class="form-control" aria-label="Answer" aria-described-by="answer">
 							</div>
+							<div id="iphone-warning">
+
+							</div>
 							<div class="input-group mb-3">
 								<div class="input-group-prepend">
 									<span class="input-group-text" id="value">Select File: </span>
@@ -187,5 +190,18 @@ else{
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	<!-- iPhone detection and warning script -->
+	<script type="text/javascript">
+		var isIPhone = navigator.userAgent.indexOf("iPhone") != -1;
+
+		if(isIPhone){
+			$('#iphone-warning').html(
+				"<div class='alert alert-danger' role='alert'>" +
+				  "<i class='fa fa-mobile'></i> - You appear to be using an iPhone. When uploading a picture, please select your picture from the gallery. DO NOT use the 'Take Photo' Function" +
+				"</div>"
+			);
+		}
+	</script>
 </body>
 </html>
